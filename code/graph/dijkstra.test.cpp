dc74cdb9c9ef9b4f94aecf7430f0ae8b357c8168
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-12-15 17:20:49 +0100

    Add dijkstra with test

---
+++ code/graph/dijkstra.test.cpp
@@ -0,0 +1,37 @@
+#include "../header.hpp"
+#include "dijkstra.cpp"
+// Test on https://open.kattis.com/problems/shortestpath1
+int main(void) {
+  ll n, m, q, s;
+  while (true)
+  {
+    cin >> n >> m >> q >> s;
+    if (n==0 && m==0 && q==0 && s==0) break;
+
+    // Create adj
+    mii adj;
+    ll v1, v2, w;
+    for (ll i = 0; i < n; i++)
+      adj.push_back(vii{});
+
+    for (ll i = 0; i < m; i++) {
+      cin >> v1 >> v2 >> w;
+      adj[v1].push_back(ii{v2, w});
+    }
+    
+    // Run single source shortest-paths dijkstra
+    vector<ll> distTo = sssp_dijkstra(adj, s);
+
+    // Run queries on distTo
+    ll queryVertex;
+    for (ll i = 0; i < q; i++) {
+      cin >> queryVertex;
+      if (distTo[queryVertex] == inf)
+        cout << "Impossible" << endl;
+      else
+        cout << distTo[queryVertex] << endl;
+    }
+    cout << endl;
+  }
+  return 0;
+}
