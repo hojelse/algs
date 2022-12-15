0598c2bf4153c0cc1c061a5cea83f1e0e8647fbd
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-12-15 17:20:40 +0100

    Add union find with test

---
+++ code/basic/union_find.cpp
@@ -0,0 +1,40 @@
+#include "../header.hpp"
+// Weighted quick-union with path compression
+struct union_find
+{
+  const int N;
+  vi p;
+
+  union_find(int n) : N(n), p(n, -1) { }
+
+  int find(int x) {
+    return p[x] < 0 ? x : p[x] = find(p[x]);
+  }
+
+  void uni(int x, int y) {
+    int i = find(x);
+    int j = find(y);
+    if (i == j) return;
+    if (p[i] < p[j]) swap(i, j);
+    p[j] += p[i];
+    p[i] = j;
+  }
+
+  void move(int s, int t) {
+    int S = find(s);
+    int T = find(t);
+    if (S == T) return;
+    int newSizeS = p[S]+1;
+    int i = 0;
+    if (s == S) // set first child of s as new root
+      for (; i < N; i++) if (p[i] == s) { S = i; break; }
+    for (; i < N; i++) if (p[i] == s) p[i] = S;
+    p[S] = newSizeS;
+    p[T]--;
+    p[s] = T;
+  }
+
+  int size(int x) {
+    return -p[find(x)];
+  }
+};
---
+++ code/basic/union_find.test.cpp
@@ -0,0 +1,18 @@
+#include "union_find.cpp"
+// Test on https://itu.kattis.com/problems/itu.disjointsets
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  auto uf = union_find(n);
+
+  int op, s, t;
+  for (int i = 0; i < m; i++)
+  {
+    cin >> op >> s >> t;
+    if (op == 0) cout << ((uf.find(s) == uf.find(t)) ? 1 : 0) << endl;
+    if (op == 1) uf.uni(s, t);
+    if (op == 2) uf.move(s, t);
+  }
+  return 0;
+}
